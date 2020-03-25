c468dd8f4f3ae82ab1660815c2f8d2732d91caa1
Garuca <g4ruc4@gmail.com>
2020-03-25 18:44:54 -0300

    Add plates olds

--- test/platesvalidator_test.dart
+++ test/platesvalidator_test.dart
@@ -1,13 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:platesvalidator/plates/plate.dart';
+import 'package:platesvalidator/plates/plates_mercosul_brazil.dart';
 
 import 'package:platesvalidator/plates_validator.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+    const ID = 'ABC1A34';
+    const ID_OLD = 'ABC1234';
+    var plate = Plate(ID_OLD).getTypePlate();
+    print(plate?.runtimeType);
+//    final calculator = Calculator();
+//    expect(calculator.addOne(2), 3);
+//    expect(calculator.addOne(-7), -6);
+//    expect(calculator.addOne(0), 1);
+//    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
   });
 }
